0d9dd9da5d7d4e1974efc23de4a85a6768a9adb6
Anton Bäckström <antonbcm@gmail.com>
2016-02-14 15:47:59 +0100

    Strassen is implemented

--- testing/antbac-python/matrixMul.py
+++ testing/antbac-python/matrixMul.py
@@ -2,19 +2,21 @@ from random import randint
 
 
 def add(m1, m2):
+    result = copy.deepcopy(m1)
     for y in range (0, len(m1)):
         for x in range (0, len(m1[0])):
-            m1[y][x] += m2[y][x]
-    return m1
+            result[y][x] += m2[y][x]
+    return result
 
 def sub(m1, m2):
+    result = copy.deepcopy(m1)
     for y in range (0, len(m1)):
         for x in range (0, len(m1[0])):
-            m1[y][x] -= m2[y][x]
-    return m1
+            result[y][x] -= m2[y][x]
+    return result
 
 
-# No error checking is made!
+# No error checking is performed!
 # Make sure the matrices are of the correct sizes
 def naive(m1, m2):
     result = []
@@ -28,37 +30,20 @@ def naive(m1, m2):
             result[y1].append(temp)
     return result
 
-# No error checking is made!
+# No error checking is performed!
 # Make sure the matrices are of the correct sizes
-# -----------------------------------------------
-# Strassen(A,B)
-# If n = 1 Output AxB
-# Else
-# Compute A(11),B(11),...,A(22),B(22) by computing m = n/2
-# P1 = Strassen(A(11),B(12)-B(22))
-# P2 = Strassen(A(11)+A(12),B(22))
-# P3 = Strassen(A(21)+A(22),B(11))
-# P4 = Strassen(A(22),B(21)-B(11))
-# P5 = Strassen(A(11)+A(22),B(11)+B(22))
-# P6 = Strassen(A(12)-A(22),B(21)+B(22))
-# P7 = Strassen(A(11)-A(21),B(11)+B(12))
-# C11 = P5+P4-P2+P6
-# C12 = P1+P2
-# C21 = P3+P4
-# C22 = P1+P5-P3-P7
-# Output C
 def strassen(m1, m2):
     if len(m1) <= 16:
         return naive(m1, m2)
     else:
-        A11 = [] # A11 = [A[i][:mid] for i in range(0, mid)]
-        A12 = []
-        A21 = []
-        A22 = []
-        B11 = []
-        B12 = []
-        B21 = []
-        B22 = []
+        A11 = [A[i][:mid] for i in range(0, mid)]
+        A12 = [A[i][mid:] for i in range(0, mid)]
+        A21 = [A[i][:mid] for i in range(mid, len(A))]
+        A22 = [A[i][mid:] for i in range(mid, len(A))]
+        B11 = [B[i][:mid] for i in range(0, mid)]
+        B12 = [B[i][mid:] for i in range(0, mid)]
+        B21 = [B[i][:mid] for i in range(mid, len(B))]
+        B22 = [B[i][mid:] for i in range(mid, len(B))]
         P1 = Strassen(A11, sub(B12,B22))
         P2 = Strassen(add(A11, A12), B22)
         P3 = Strassen(add(A21, A22), B11)
@@ -70,8 +55,11 @@ def strassen(m1, m2):
         C12 = add(P1, P2)
         C21 = add(P3, P4)
         C22 = sub(sub(add(P1, P5), P3), P7)
-        C = []
-        return C
+        C11.extend(C21)
+        C12.extend(C22)
+        for i in range(0, len(C11)):
+            C11[i].extend(C12[i])
+        return C11
 
 
 def generate(x, y):
@@ -79,7 +67,7 @@ def generate(x, y):
     for i in range (0, x):
         matrix.append([])
         for j in range (0, y):
-            matrix[i].append(randint(0, 100))
+            matrix[i].append(randint(0, 10))
     return matrix
 
 
@@ -90,8 +78,13 @@ def printMatrix(matrix):
         print()
     print()
 
-a = generate(128,128)
-b = generate(128,128)
+a = generate(4,4)
+b = generate(4,4)
+print("a")
 printMatrix(a)
+print("b")
 printMatrix(b)
+print("naive")
 printMatrix(naive(a, b))
+print("strassen")
+printMatrix(strassen(a, b))
