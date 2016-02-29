3588c4b5c6ceca12633550bd2aafce15f1730cf3
Robert <avacore1337@gmail.com>
2016-02-29 10:04:08 +0100

    added a tiled version, not tested yet

--- testing/robertwb-c/naive.c
+++ testing/robertwb-c/naive.c
@@ -9,12 +9,20 @@ void matrixMultiplication(uint64_t **A, uint64_t **B, uint64_t **C, size_t size)
       C[i][j] = sum;
     }
   }
+  return;
+}
 
-  // for i in range(n):
-  //     for j in range(p):
-  //         theSum = 0
-  //         for k in range(m):
-  //             theSum += A[i][k] * B[k][j]
-  //         C[i][j] = theSum
+void matrixMultiplicationTiled(uint64_t **A, uint64_t **B, uint64_t **C, size_t size){
+  for (size_t i = 0; i < size; i += 4) {
+    for (size_t j = 0; j < size; j += 4) {
+      for (size_t k = 0; k < size; k++) {
+        for (size_t x = 0; x < i + 4; x++) {
+          for (size_t y = 0; y < j + 4; y++) {
+            C[x][y] += A[x][k] * B[k][y];
+          }
+        }
+      }
+    }
+  }
   return;
 }
--- testing/robertwb-c/naive.h
+++ testing/robertwb-c/naive.h
@@ -5,5 +5,6 @@
 #include <stdlib.h>
 #include "util.h"
 
-void matrixMultiplication(uint64_t **, uint64_t **, uint64_t **, size_t size);
+void matrixMultiplication(uint64_t **, uint64_t **, uint64_t **, size_t);
+void matrixMultiplicationTiled(uint64_t **, uint64_t **, uint64_t **, size_t);
 #endif // FOO_GUARD_FOR_MYRANDOM
