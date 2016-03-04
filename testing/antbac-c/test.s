c76cc71eebff1d7d953c42731453fa9656612a6a
Anton Bäckström <antonbcm@gmail.com>
2016-03-04 13:05:06 +0100

    Changed some comments

--- testing/antbac-c/test.s
+++ testing/antbac-c/test.s
@@ -11,10 +11,10 @@
 # 5 : %r8
 # 6 : %r9
 #
-# %xmm0-7 (128 bit, lower half of ymmX) 
+# %xmm0-7 (128 bit, lower half of ymmX)
 # %ymm0-7 (256 bit)
 #
-# Return : %eax or %rax
+# Return : %eax for an int or %rax for a long
 #
 
 .section .text
@@ -26,7 +26,7 @@
 # %rdx = pointer to b-array
 vec_mul:
 vmovapd (%rdx),%ymm0      # Copy b-array to ymm0
-vmulpd (%rsi),%ymm0,%ymm0 # Multiply a-array into ymmo
+vmulpd (%rsi),%ymm0,%ymm0 # Multiply a-array into ymm0
 vmovapd %ymm0,(%rdi)      # Move the multiplied array into the r-array (this causes an error)
-vzeroupper                # Set the top 128 bits of all ymmX registers to 0 for some reason, probably a convention
+vzeroupper                # Set the top 128 bits of all ymmX registers to 0, probably a convention
 retq
